3eee1ea6f873ee84fe9639e4ab8f0396d379a2be
Darshan R.M <=>
2024-04-02 11:29:57 +0530

    logger working file

--- src/logger.py
+++ src/logger.py
@@ -3,12 +3,12 @@ import os
 from datetime import datetime
 
 LOG_FOLDER_NAME = f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}'
-logs_folder = os.path.join(os.getcwd(), 'logs', LOG_FOLDER_NAME)
-os.makedirs(logs_folder, exist_ok = True)
+logs_folder_path = os.path.join(os.getcwd(), 'logs', LOG_FOLDER_NAME)
+os.makedirs(logs_folder_path, exist_ok = True)
 
 logs_file_name = f'{LOG_FOLDER_NAME}.log'
 
-LOG_FILE_PATH = os.path.join(logs_folder, logs_file_name)
+LOG_FILE_PATH = os.path.join(logs_folder_path, logs_file_name)
 
 logging.basicConfig(
     filename = LOG_FILE_PATH,
