4bddc5237d351427bf3435f5ec047f8a2d92255f
Darshan R.M <=>
2024-04-02 21:47:16 +0530

    working fine

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -55,8 +55,7 @@ class PredictPipelineConfig:
     preprocessor_path:str = os.path.join('artifacts', 'preprocessor.pkl')
     model_path:str = os.path.join('artifacts', 'model.pkl')
 
-class PredictPipeline:
-    
+class PredictPipeline:    
     def __init__(self):
         self.prediction_pipeline_config = PredictPipelineConfig()
     
@@ -71,11 +70,19 @@ class PredictPipeline:
             scaled_data = preprocessor.transform(features)
             logging.info(f'Scaled input data')
 
-            pred = model.predict(scaled_data)[0]
-            logging.info(f'Predicted output is {pred}')
+            prediction = model.predict(scaled_data)[0]
+            logging.info(f'Predicted output is {prediction}')
             
             logging.info('Prediction pipeline completed')
-            return pred        
+            return prediction        
         except Exception as e:
             logging.info('!!! Error occured in prediction pipeline')
             raise CustomException(e, sys)
+
+
+# for testing purpose
+# if __name__ == '__main__':
+#     custom_data_obj = CustomData(1.08,62.7,55.0,6.53,6.59,4.09,'Ideal','H','VS2')
+#     df = custom_data_obj.get_data_as_dataframe()
+#     prediction_obj = PredictPipeline()
+#     print(prediction_obj.predict(df))
