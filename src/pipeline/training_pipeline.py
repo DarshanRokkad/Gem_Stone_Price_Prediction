d76370b2f2027ee5692bee509c9c334e9f8e208d
Darshan R.M <=>
2024-04-02 21:31:01 +0530

    working fine

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -11,16 +11,24 @@ from components.model_evaluation import ModelEvaluation
 
 logging.info('Training pipeline started')
 
-data_ingestion = DataIngestion()
-train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+try : 
+    logging.info('Data ingestion initiated')
+    data_ingestion = DataIngestion()
+    train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
-data_transformation = DataTransformation()
-train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    logging.info('Data transformation initiated')
+    data_transformation = DataTransformation()
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
-model_trainer = ModelTrainer()
-model_trainer.initiate_model_training(train_arr, test_arr)
+    logging.info('Model trainer initiated')
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
 
-model_evaluation = ModelEvaluation()
-model_evaluation.initiate_model_evaluation(train_arr, test_arr)
+    logging.info('Model evaluator initiated')
+    model_evaluation = ModelEvaluation()
+    model_evaluation.initiate_model_evaluation(train_arr, test_arr)
+except Exception as e:
+    logging.info('!!! Error inside training pipeline')
+    CustomException(e, sys)
 
 logging.info('Training pipeline completed')
