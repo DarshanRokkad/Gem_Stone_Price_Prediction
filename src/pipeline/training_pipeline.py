1072ce16dbd2032d40f139d84ceae66450293900
Darshan R.M <=>
2024-04-03 22:13:21 +0530

    training pipeline

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -7,26 +7,58 @@ from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 from src.components.model_evaluation import ModelEvaluation
 
-logging.info('Training pipeline started')
 
-try : 
-    logging.info('Data ingestion initiated')
-    data_ingestion = DataIngestion()
-    train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+class TrainingPipeline:
+    def start_data_ingestion(self):
+        try:
+            data_ingestion = DataIngestion()
+            train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+            return train_data_path, test_data_path
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def start_data_transformation(self, train_data_path, test_data_path):
+        try:
+            data_transformation = DataTransformation()
+            train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+            return train_arr, test_arr
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def start_model_trainer(self, train_arr, test_arr):
+        try:
+            model_trainer = ModelTrainer()
+            model_trainer.initiate_model_training(train_arr, test_arr)
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def start_model_evaluation(self, train_arr, test_arr):
+        try:
+            model_evaluation = ModelEvaluation()
+            model_evaluation.initiate_model_evaluation(train_arr, test_arr)
+        except Exception as e:
+            raise CustomException(e, sys)
 
-    logging.info('Data transformation initiated')
-    data_transformation = DataTransformation()
-    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    def train_model(self):
+        try:
+            logging.info('Training pipeline started')
+            
+            logging.info('Data ingestion initiated')
+            train_data_path,test_data_path=self.start_data_ingestion()
+            logging.info('Data transformation initiated')            
+            train_arr,test_arr=self.start_data_transformation(train_data_path,test_data_path)
+            logging.info('Model training initiated')            
+            self.start_model_trainer(train_arr,test_arr)
+            logging.info('Model evaluation initiated')            
+            self.start_model_evaluation(train_arr,test_arr)
+            
+            logging.info('Training pipeline completed')
+        except Exception as e:
+            logging.info('!!! Error inside training pipeline')
+            raise CustomException(e, sys)
 
-    logging.info('Model trainer initiated')
-    model_trainer = ModelTrainer()
-    model_trainer.initiate_model_training(train_arr, test_arr)
 
-    logging.info('Model evaluator initiated')
-    model_evaluation = ModelEvaluation()
-    model_evaluation.initiate_model_evaluation(train_arr, test_arr)
-except Exception as e:
-    logging.info('!!! Error inside training pipeline')
-    CustomException(e, sys)
-
-logging.info('Training pipeline completed')
+# for testing purpose
+# if __name__ == '__main__':
+#     training_pipeline = TrainingPipeline()
+#     training_pipeline.train_model()
