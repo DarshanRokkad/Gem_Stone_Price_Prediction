7ec10d8ae7e3d154b5051620f01036b9e3e714ab
Darshan R.M <=>
2024-04-05 13:44:47 +0530

    u

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def api_testing():
         return jsonify(prediction)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug = True)
+    app.run(host = '0.0.0.0', port = 8000, debug = True)
